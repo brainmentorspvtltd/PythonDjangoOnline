e7575b95f48ca1719c72efe8c4842c5ca0a9f9bb
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2019-03-19 13:02:47 +0530

    Add files via upload

--- DjangoCodes/OnlineShop/webapp/urls.py
+++ DjangoCodes/OnlineShop/webapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('products/', views.products, name='products'),
+    path('mobiles/',views.mobiles, name='mobiles'),
+    path('contact/', views.contact, name='contact'),
+    path('about/',views.about, name='about')
 ]
